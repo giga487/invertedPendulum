8fc455c18c24787477ed75a0d5a1beb45bcf867b
giga487 <giga487@hotmail.com>
2018-11-10 12:24:57 +0100

    Equazioni della Dinamica e funzioni

---
+++ Eq_Dinamica.m
@@ -0,0 +1,43 @@
+
+function [B,C,G] = Eq_Dinamica(ms,mr,L,R,phi)
+% Equazioni della dinamica
+% Scrivo le equazioni del moto nella forma B*ddot(q)+C*dot(q)+G = T
+% dove B forze di inerzia corrispondenti ad accelerazioni tangenziali
+% C forze d'inerzia corrispondenti ad accelerazioni centrifughe e di Coriolis
+% G forze gravitazionali e/o elastiche in dipendenza della forma di U
+% T forze generazlizzate attive non conservative degli attuatori sui giunti
+
+% K = 1/2*ms*vB^2 + 1/2*mr*vG^2 + 1/2*Is*dot(psi)+1/2*Id*(dot(psi)-dot(tetha))^2;
+% Visto che si ipotizza la ruota più cava possibile con le masse
+% disposte maggiormente lungo il bordo Is diviene:
+% Is = mr*R^2; 
+% Id = 1/12*ms*L^2;
+% la velocità vB = L/2*\dot(psi)*[-sin(psi); cos(psi); 0];
+% mentre la vG = 2*vB;
+% vB^T*vB = L^2/4*\dot(psi)^2 e vG = L^2*\dot(psi)^2
+% allora K diventa:
+% K = % 1/2*ms*L^2/4*\dot(psi)^2+1/2*mr*L^2*\dot(psi)^2+
+% 1/2*1/24*L^2*\dot(psi)^2+
+% 1/2*mr*R^2*(\dot(psi)^2+\dot(theta)^2-2(\dot(psi)*\dot(theta));
+% allora B, ottenuta dalla derivazione rispetto ad una variabile \dot(psi)
+% e \dot(theta) diviene:
+
+g = 9.81; %gravità generica, non localizzata, servirebbe farlo [m/s^2]
+
+B11 = 1/4*ms*L*L+mr*L*L+0.0833*ms*L*L+mr*R*R;
+B12 = -mr*R*R;
+B21 = -mr*R*R;
+B22 =  mr*R*R;
+
+G1 = -ms*g*L/2*sind(phi)-mr*g*L*sind(phi);
+G2 = 0;
+
+B = [B11 B12;
+     B21 B22];
+ 
+C = [0;0];
+
+G = [ G1;
+      G2 ]; 
+
+end
--- main.m
+++ main.m
@@ -1,20 +1,11 @@
 % Questo Pendolo è monodimensionale
-% Scrivo le equazioni del moto nella forma B*ddot(q)+C*dot(q)+G = T
-% dove B forze di inerzia corrispondenti ad accelerazioni tangenziali
-% C forze d'inerzia corrispondenti ad accelerazioni centrifughe e di Coriolis
-% G forze gravitazionali e/o elastiche in dipendenza della forma di U
-% T forze generazlizzate attive non conservative degli attuatori sui giunti
-B11 = ms*L^2/4+ms*L^2/12+mr*L^2+1/2*mr*R^2;
-B12 = -1/2*mr*R^2;
-B21 = -1/2*mr*R^2;
-B22 = +1/2*mr*R^2;
-G1 = -ms*g*L/2*sind(phi)-mr*g*L*sind(phi);
-G2 = 0;
 
-B = [B11 B12;
-     B21 B22];
- 
-C = [0;0];
+ms = 1;%massa asta [Kg]
+mr = 1;%massa rotore [Kg]
+L =  1;%braccio pendolo [m]
+R =  1;%raggio della ruota inerziale [m]
+%Phi è immesso nella dinamica in modo da calcolare la posizione senza
+%linearizzazione.
+phi = 0;%è l'angolo di inclinazione relativo all'asse Y [deg]. ATTENZIONE
 
-G = [ G1;
-      G2 ]; 
+[B,C,G] = Eq_Dinamica(ms,mr,L,R,phi);
