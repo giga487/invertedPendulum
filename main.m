564d1eadc63c8de62d2a40d021d2f68ed5a9bd97
giga487 <giga487@hotmail.com>
2018-11-15 22:03:07 +0100

    Simulazione Simulink con unita di misura corrette

--- main.m
+++ main.m
@@ -4,8 +4,8 @@ clc;
 clear;
 close all;
 
-ms = 1;%massa asta [Kg]
-mr = 1;%massa rotore [Kg]
+ms = 0.5;%massa asta [Kg]
+mr = 2;%massa rotore [Kg]
 L =  0.5;%braccio pendolo [m]
 R =  0.15;%raggio della ruota inerziale [m]
 g = 9.81; %gravità m/s^2
@@ -19,8 +19,12 @@ G1 = (-ms*g*L/2-mr*g*L);
 
 [A_l,B_l,C_l,D_l] = SSDinamica(B,C,G1);
 
-G = ss(A_l,B_l,C_l,D_l);
+G = ss(A_l,B_l,C_l,D_l,'TimeUnit','seconds','Ts',0.01);
+
 G.StateName = {'phi','dot_phi'};
 G.InputName = {'Coppia'};
 
 
+%% Parametri Motori
+Wsat = 122*2*pi/60;
+Tsat = 36*100;
