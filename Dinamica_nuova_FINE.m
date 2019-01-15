ec62ad8d4e966477e1f3402a3d314ca2a16ca6d8
Pierluigi Amato <amato.pierluigi@gmail.com>
2019-01-15 19:30:22 +0100

    Dimensionamento Motore Caso Limite

--- Dinamica_nuova_FINE.m
+++ Dinamica_nuova_FINE.m
@@ -4,13 +4,17 @@ close all;
 
 %Evidenzio la dinamica del pendolo inverso
 L = 0.25; %lunghezza del pendolo.
-diametro_ruota = 0.15;
-m_ruota = 0.457;
-I_ruota = m_ruota*(diametro_ruota/2)^2;
+% diametro_ruota = 0.15;
+% m_ruota = 0.457;
+diametro_ruota = 0.09; %m
+m_ruota = 0.141061;
 m_mot = 0.333;
-I_mot = L^2*m_mot;
 m_asta = 0.05;
+I_ruota = 0.0494; %SOLIDOWORKS
+I_ruota_stima = m_ruota*(diametro_ruota/2)^2;
+I_mot = L^2*m_mot;
 I_asta = m_asta*(L^2)/12;
+
 g = 9.81; %m/s^2
 
 
@@ -57,46 +61,50 @@ NOISE_rms = 0.02*pi/180; %rad
 
 %% Calcolo del sistema in forma di quasi velocita
 
-Kp = -250;
-Kd = -10
+Kp = -0.01;
+Kd = 0;
 %con questi due parametri il sistema funziona.
 %I risultati sono una coppia massima 
 
 %% Parametri Motori
 
-Coppia_stalloKgm = 36*0.01/2;  %kg*m
+Coppia_stallo_Kgm_MAX = 36*0.01;  %kg*m
+Coppia_stalloKgm = Coppia_stallo_Kgm_MAX;  %kg*m
 %Il kilogrametro è un unità di misura tecnica dell'energia.
 %un kgm è pari ad un kg forza per un metro.
 %Il chilogrammetro equivale quindi al lavoro necessario per sollevare
 %di un metro un corpo di massa di un chilogrammo.
 %devo moltiplicare quindi per 9.81 per ottenere Nm
-Coppia_stalloNm = Coppia_stalloKgm*9.81;
+Coppia_stalloNm_TOT = Coppia_stalloKgm*9.81;
+Coppia_stalloNm = Coppia_stalloNm_TOT/2;
 V_max = 12; %V
 I_stallo = 6.5; %A DI STALLO
 I_max_NOLOAD = 0.25; %A
 w_max = 122*2*pi/60/2; %rad/s
 
 R_max = V_max/I_max_NOLOAD;
-K = V_max/w_max;
 
-b = 0.1; %N*m*s
 L = 2*0.001; %mH
-
-%% Motore
 
 s = tf('s');
 b = 0.1; %attrito viscoso
-K = 0.01;
-R = 15; %CASO PEGGIORE di stallo del motore
+r_peggiore = 3;
+K = (V_max-I_max_NOLOAD*r_peggiore)/w_max
+R = r_peggiore; %CASO PEGGIORE di stallo del motore
 
 g = K/((I_ruota*s+b)*R+K^2)
-
+g2 = 0.5/(s+3);
 Motore_num = K;
 Motore_den = [I_ruota*R, b*R+K^2];
 
+num_dc = K;
+den_dc = [(I_ruota*L) ((I_ruota*R)+(L*b)) ((b*R)+K^2)];
+dcmotor = tf(num_dc,den_dc)
 
-figure(1);bode(g)
 
-g2 = 10/(s+100)
-
-figure(2);bode(g2)
+damp(g)
+figure(1);
+subplot(1,2,1);
+bode(dcmotor);grid on;
+subplot(1,2,2);
+bode(g2);grid on;
